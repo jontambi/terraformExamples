c33e4cd98b297ba4fad818ae49c672a75557042f
John Tambi <jon.tambi@gmail.com>
2019-08-15 23:20:19 -0500

    output variable will show the new output

--- main.tf
+++ main.tf
@@ -33,3 +33,7 @@ variable "server_port" {
   description = "The port the server will use for HTTP requests"
   default = 8080
 }
+
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
+}
